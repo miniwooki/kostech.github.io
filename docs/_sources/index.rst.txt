1d38fc47afd4e74ff50b08489c7d5a9d52b3f03d
cwjun <kangsia@live.co.kr>
2023-02-22 12:16:37 +0900

    init

---
+++ docs/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. kostech_docs documentation master file, created by
+   sphinx-quickstart on Wed Feb 22 11:53:20 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to kostech_docs's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'kostech_docs'
+copyright = '2023, cwjun'
+author = 'cwjun'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+## 추가
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/my_package'))
