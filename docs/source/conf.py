d3bb9c1ea7daa5e7d54f8a0fe40b5b24a1926c65
cwjun <kangsia@live.co.kr>
2023-04-04 12:44:55 +0900

    none

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ author = 'cwjun'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['myst_parser']
-myst_heading_anchors = 2
+myst_heading_anchors = 1
 
 templates_path = ['_templates']
 exclude_patterns = []
