e4e5b6398fcbbdfae33b14d3ed08e089f6ff8b75
cwjun <kangsia@live.co.kr>
2023-04-04 14:14:58 +0900

    none

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ myst_heading_anchors = 3
 # myst_all_links_external = True
 templates_path = ['_templates']
 exclude_patterns = []
-myst_enable_extensions = ["colon_fence"]
+myst_enable_extensions = ["colon_fence", "attrs_inline"]
 
 
 # -- Options for HTML output -------------------------------------------------
